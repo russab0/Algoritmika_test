df18a5030dea957c69dc5adbdc783eddf5c1c5fa
Ruslan Sabirov <r.sabirov@innopolis.ru>
2018-11-14 19:14:33 +0300

    Task done

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Algoritmika_test
 Test task for teachers of "Algoritmika" programming school
+
+Program will ask you to enter number of figures and line color and then will draw them
---
+++ drawing_figures.py
@@ -0,0 +1,50 @@
+import turtle
+
+
+class Length: # Class for refference to lenght value
+    def __init__(self, l):
+        self.l = l
+
+    def get(self):
+        return self.l
+
+    def set(self, l):
+        self.l = l
+
+
+class Draw:
+    RUS_ENG_COLOR = {"зеленый": "green",
+                     "синий": "blue",
+                     "default": "black"} # Constant for converting Russian color names to their English analogue
+
+    def get_eng_color(self, rus): # Function that returns English name of given in Russain color
+        if rus in self.RUS_ENG_COLOR: # Color is defined
+            return self.RUS_ENG_COLOR[rus]
+        return self.RUS_ENG_COLOR["default"]
+
+    def __init__(self, count, color):
+        assert 1 <= count <= 6, "Unacceptable number of figures" 
+
+        length = Length(150).get() # Variable that points to 150
+        turtle.color(self.get_eng_color(color)) # Assigning right color
+
+        turtle.left(30) # Initial rotation
+        for i in range(count): # Loop for pointning figures `count` time
+            for j in range(4): # Loop for each side of rectangle
+                turtle.forward(length)
+                turtle.left(90)
+            turtle.right(60) # Preparing for next iteration
+        turtle.exitonclick() # To prevent closing right after drawing
+
+
+def main():
+    ts = turtle.getscreen()
+    ts.title = "Drawing figures" # Setting title of window
+    number = int(ts.numinput("Фигуры", "Введите количество фигру: 1-6", default=6, minval=1, maxval=6)) # Reading number
+    color = ts.textinput("Цвет", "Выберите цвет: синий, зеленый") # Readlng color
+
+    Draw(number, color) 
+
+
+if __name__ == "__main__": # If program exetuted directly (not imported)
+    main()
